63abc539afb75baae26a4d4b0fd1c302126b4c97
Vitor Cortez <vitor.a.cortez@gmail.com>
2017-12-22 19:41:26 -0300

    Add initial Menu schema

---
+++ models/menu.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const RestaurantSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  open: { type: Boolean, required: true },
+  known: { type: Boolean, required: true },
+  meals: mongoose.SchemaTypes.Mixed
+})
+
+const DaySchema = new mongoose.Schema({
+  day: { type: Number, min: 0, max: 6 },
+  lunch: [RestaurantSchema],
+  dinner: [RestaurantSchema]
+})
+
+const MenuSchema = new mongoose.Schema({
+  monday: DaySchema,
+  tuesday: DaySchema,
+  wednesday: DaySchema,
+  thursday: DaySchema,
+  friday: DaySchema,
+  saturday: DaySchema,
+  publisher: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  startDate: Date,
+  endDate: Date
+})
+
+const Menu = mongoose.model('Menu', MenuSchema)
+
+module.exports = Menu
