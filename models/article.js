b7145573f08c5ca4ba04aefebc661747f4d5b451
Vitor Cortez <vitor.a.cortez@gmail.com>
2017-12-27 16:21:49 -0300

    Add Article schema

---
+++ models/article.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+const ArticleSchema = new mongoose.Schema({
+  code: { type: Number, unique: true },
+  title: { type: String, required: true },
+  link: { type: String, required: true },
+  text: [String],
+}, { timestamps: true })
+
+ArticleSchema.statics.insertFromScraper = function(json, done) {
+  json.forEach((doc) => {
+    doc.text = doc.text.filter(p => p.length > 0)
+  })
+
+  const newCodes = json.map(doc => doc.code)
+
+  this
+    .find({})
+    .where('code')
+    .in(newCodes)
+    .select('code')
+    .then((articles) => {
+      const prevCodes = articles.map(a => a.code)
+
+      const newDocs = json
+        .filter(d => !prevCodes.includes(d.code))
+        .map(v => {
+          let t = v.time.split('h')
+          let d = v.date.split('/')
+
+          let b = new Date('2000-01-01T00:00:00-03:00')
+          b.setFullYear(2000 + Number(d[2]), Number(d[1]) - 1, Number(d[0]))
+          b.setHours(Number(t[0]))
+          b.setMinutes(Number(t[1]))
+          v.createdAt = b
+          delete v.date
+          delete v.time
+          return v
+        })
+
+      this.insertMany(newDocs, (err, added) => {
+        if (err) {
+          return done(err)
+        }
+
+        done(null, added)
+      })
+    })
+    .catch(done)
+}
+
+module.exports = mongoose.model('Article', ArticleSchema)
